173a73ccef8026b11adca5c1fd8aeb56df5a39ca
Michal Čihař <michal@cihar.com>
2003-12-04 22:25:28 +0000

    [wammu @ michal@cihar.com--2003-main/wammu--devel--0.1--patch-6] added support for sorting Implemented sorting by sorting list of values and Pythons internal sort.

--- Wammu/Browser.py
+++ Wammu/Browser.py
@@ -10,6 +10,7 @@ class Browser(wx.ListCtrl, wx.lib.mixins.listctrl.ListCtrlAutoWidthMixin):
     def __init__(self, parent, win):
         wx.ListCtrl.__init__(self, parent, -1,
                             style=wx.LC_REPORT|wx.LC_VIRTUAL|wx.LC_HRULES|wx.LC_VRULES)
+#                            style=wx.LC_REPORT|wx.LC_HRULES|wx.LC_VRULES)
         self.win = win
 
         self.attr1 = wx.ListItemAttr()
@@ -19,6 +20,7 @@ class Browser(wx.ListCtrl, wx.lib.mixins.listctrl.ListCtrlAutoWidthMixin):
 
         wx.EVT_LIST_ITEM_SELECTED(self, self.GetId(), self.OnItemSelected)
         wx.EVT_LIST_ITEM_ACTIVATED(self, self.GetId(), self.OnItemActivated)
+        wx.EVT_LIST_COL_CLICK(self, self.GetId(), self.OnColClick)
 ##        wx.lib.mixins.listctrl.ColumnSorterMixin
         wx.lib.mixins.listctrl.ListCtrlAutoWidthMixin.__init__(self)
 
@@ -26,6 +28,7 @@ class Browser(wx.ListCtrl, wx.lib.mixins.listctrl.ListCtrlAutoWidthMixin):
         if self.type == 'info':
             self.InsertColumn(0, 'Name')
             self.InsertColumn(1, 'Value')
+            self.sortkeys = (0, 1)
 #            self.SetColumnWidth(0, 100)
 #            self.SetColumnWidth(1, 200)
             self.SetColumnWidth(0, wx.LIST_AUTOSIZE)
@@ -35,6 +38,7 @@ class Browser(wx.ListCtrl, wx.lib.mixins.listctrl.ListCtrlAutoWidthMixin):
             self.InsertColumn(1, 'Memory')
             self.InsertColumn(2, 'Name')
             self.InsertColumn(3, 'Number')
+            self.sortkeys = ('Location', 'MemoryType', 'Name', 'Number')
             self.SetColumnWidth(0, wx.LIST_AUTOSIZE)
             self.SetColumnWidth(1, wx.LIST_AUTOSIZE)
             self.SetColumnWidth(2, wx.LIST_AUTOSIZE)
@@ -44,19 +48,40 @@ class Browser(wx.ListCtrl, wx.lib.mixins.listctrl.ListCtrlAutoWidthMixin):
             self.InsertColumn(1, 'Type')
             self.InsertColumn(2, 'Name')
             self.InsertColumn(3, 'Number')
+            self.sortkeys = ('Location', 'MemoryType', 'Name', 'Number')
             self.SetColumnWidth(0, wx.LIST_AUTOSIZE)
             self.SetColumnWidth(1, wx.LIST_AUTOSIZE)
             self.SetColumnWidth(2, wx.LIST_AUTOSIZE)
             self.SetColumnWidth(3, wx.LIST_AUTOSIZE)
         self.resizeLastColumn(0)
     
+    def Sorter(self, i1, i2):
+        return self.sortorder * cmp(i1[self.sortkey], i2[self.sortkey])
+    
     def Change(self, type, values):
         self.type = type
         self.values = values
+        self.sortkey = ''
         self.ClearAll()
-        self.SetItemCount(len(self.values))
+        self.SetItemCount(len(values))
         self.ShowHeaders()
+        self.Resort(0)
 
+    def Resort(self, col):
+        nextsort = self.sortkeys[col]
+        if nextsort == self.sortkey:
+            self.sortorder = -1 * self.sortorder
+        else:
+            self.sortorder = 1
+        self.sortkey = nextsort
+        self.values.sort(self.Sorter)
+        # FIXME icon in headers
+        top = self.GetTopItem() 
+        self.RefreshItems(top, top + self.GetCountPerPage())
+
+    def OnColClick(self, evt):
+        self.Resort(evt.GetColumn())
+        
     def OnItemSelected(self, event):
         evt = Wammu.Events.ShowEvent(index = event.m_itemIndex)
         wx.PostEvent(self.win, evt)
@@ -70,6 +95,7 @@ class Browser(wx.ListCtrl, wx.lib.mixins.listctrl.ListCtrlAutoWidthMixin):
         return item.GetText()
 
 
+
     def OnGetItemText(self, item, col):
         if self.type == 'info':
             return self.values[item][col]
@@ -80,10 +106,8 @@ class Browser(wx.ListCtrl, wx.lib.mixins.listctrl.ListCtrlAutoWidthMixin):
                 return self.values[item]['MemoryType']
             elif col == 2:
                 return self.values[item]['Name']
-#                return Wammu.Utils.GetMemoryEntryName(self.values[item])
             else:
                 return self.values[item]['Number']
-#                return Wammu.Utils.GetMemoryEntryNumber(self.values[item])
         elif self.type == 'call':
             if col == 0:
                 return self.values[item]['Location']
@@ -91,10 +115,8 @@ class Browser(wx.ListCtrl, wx.lib.mixins.listctrl.ListCtrlAutoWidthMixin):
                 return self.values[item]['MemoryType']
             elif col == 2:
                 return self.values[item]['Name']
-#                return Wammu.Utils.GetMemoryEntryName(self.values[item])
             else:
                 return self.values[item]['Number']
-#                return Wammu.Utils.GetMemoryEntryNumber(self.values[item])
                 
         else:
             return 'Item %d, column %d' % (item, col)
