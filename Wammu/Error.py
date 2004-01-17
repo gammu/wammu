31e61774b00746cf10ccc2f89f34c7739dc0ed39
Michal Čihař <michal@cihar.com>
2004-01-17 11:43:02 +0000

    [wammu @ michal@cihar.com--2003-main/wammu--devel--0.4--patch-29] more info in error message more info in error message

--- Wammu/Error.py
+++ Wammu/Error.py
@@ -1,11 +1,18 @@
+import Wammu
+import gammu
 import traceback,sys
 
 def Handler(type, value, tback):
     """User friendly error handling """
+    print '--------------------- Version ---------------------'
+    print 'Wammu        %s' % Wammu.__version__
+    print 'python-gammu %s' % gammu.Version()[1]
+    print 'Gammu        %s' % gammu.Version()[0]
     print '-------------------- Traceback --------------------'
     traceback.print_exception(type, value, tback)
     print '---------------------------------------------------'
     if type == UnicodeEncodeError:
-        print 'Unicode encoding error appeared, see question 1 in FAQ, how to avoid this.'
+        print 'Unicode encoding error appeared, see question 1 in FAQ, how to solve this.'
         print
-    print 'You have probably found a bug. You might help improving this software by sending above text and description how it was caused to michal@cihar.com. Thanks.'
+    print 'You have probably found a bug. You might help improving this software by'
+    print 'sending above text and description how it was caused to michal@cihar.com.'
