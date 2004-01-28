a62513f6c45828e1c02ea647e5c0af24fa3ce800
Michal Čihař <michal@cihar.com>
2004-01-28 12:51:22 +0000

    [wammu @ michal@cihar.com--2004-main/wammu--devel--0.5--patch-5] added doc strings added doc strings

--- Wammu/__init__.py
+++ Wammu/__init__.py
@@ -15,7 +15,8 @@
 # this program; if not, write to the Free Software Foundation, Inc., 59 Temple
 # Place, Suite 330, Boston, MA 02111-1307 USA
 
-"""Wammu modules
+"""
+Wammu modules
 """
 
 import sys
