dcf13b090c50a2e163af35d0fdde2cdaf9a64fa2
Michal Čihař <michal@cihar.com>
2004-02-12 20:01:09 +0000

    [wammu @ michal@cihar.com--2004-main/wammu--devel--0.5--patch-29] fixed typo in header fixed typo in header

--- Wammu/Main.py
+++ Wammu/Main.py
@@ -7,7 +7,7 @@
 # any later version.
 #
 # This program is distributed in the hope that it will be useful, but WITHOUT
-# ANY WARRANTY; without even the implied warranty of MER- CHANTABILITY or
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
 # FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
 # more details.
 #
