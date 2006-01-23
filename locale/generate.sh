8d8b9ba4e81573590f69aebaf94ca7960352fe71
Michal Čihař <michal@cihar.com>
2006-01-23 06:32:37 +0000

    [wammu @ michal@cihar.com--2005-main/wammu--devel--1--patch-68] resort localisations - localisations are now sorted by text

--- locale/generate.sh
+++ locale/generate.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-for loc in cs de fr hu it ; do
+. locale/list.sh
+for loc in $LOCS ; do
     mkdir -p locale/$loc/LC_MESSAGES
     echo -n "$loc: "
     msgfmt --statistics -o locale/$loc/LC_MESSAGES/wammu.mo locale/$loc.po
--- locale/update.sh
+++ locale/update.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-xgettext -d wammu -F --msgid-bugs-address=michal@cihar.com -o locale/wammu.pot `find Wammu/ -name '*.py'`
-for loc in cs de fr hu it ; do
-    msgmerge -F -U locale/$loc.po locale/wammu.pot
+. locale/list.sh
+xgettext -s -d wammu --msgid-bugs-address=michal@cihar.com -o locale/wammu.pot `find Wammu/ -name '*.py'`
+for loc in $LOCS ; do
+    msgmerge -s -U locale/$loc.po locale/wammu.pot
 done
