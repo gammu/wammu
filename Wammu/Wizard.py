ca38602e90832d3455281a9faf36a36cbc6c163e
Michal Čihař <michal@cihar.com>
2006-11-24 18:15:59 +0000

    [wammu @ michal@cihar.com--2005-main/wammu--devel--1--patch-194] improvements to wizard

---
+++ Wammu/Wizard.py
@@ -0,0 +1,101 @@
+# -*- coding: UTF-8 -*-
+# vim: expandtab sw=4 ts=4 sts=4:
+'''
+Wammu - Phone manager
+Wizard helper classes
+'''
+__author__ = 'Michal Čihař'
+__email__ = 'michal@cihar.com'
+__license__ = '''
+Copyright (c) 2003 - 2006 Michal Čihař
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License version 2 as published by
+the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+more details.
+
+You should have received a copy of the GNU General Public License along with
+this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+'''
+
+import wx
+import wx.wizard
+import Wammu.Paths
+from Wammu.Utils import StrConv, Str_ as _
+
+
+class SimplePage(wx.wizard.PyWizardPage):
+    """
+    Simple wizard page with unlimited rows of text.
+    """
+    def __init__(self, parent, titletext, bodytext = None, detailtexts = None):
+        wx.wizard.PyWizardPage.__init__(self, parent)
+        self.sizer = wx.BoxSizer(wx.VERTICAL)
+        self.SetSizer(self.sizer)
+        title = wx.StaticText(self, -1, titletext)
+        title.SetFont(wx.Font(18, wx.SWISS, wx.NORMAL, wx.BOLD))
+        self.sizer.Add(title, 0, wx.ALIGN_CENTRE|wx.ALL, 5)
+        self.sizer.Add(wx.StaticLine(self, -1), 0, wx.EXPAND|wx.ALL, 5)
+        self.prev = None
+        self.next = None
+        if bodytext is not None:
+            body = wx.StaticText(self, -1, bodytext)
+            body.Wrap(400)
+            self.sizer.Add(body, 0, wx.ALL, 5)
+        if detailtexts is not None:
+            for row in detailtexts:
+                detail = wx.StaticText(self, -1, row)
+                detail.Wrap(400)
+                self.sizer.Add(detail, 0, wx.ALL, 5)
+
+    def SetNext(self, next):
+        self.next = next
+
+    def SetPrev(self, prev):
+        self.prev = prev
+
+    def GetNext(self):
+        return self.next
+
+    def GetPrev(self):
+        return self.prev
+
+class ChoicePage(SimplePage):
+    """
+    Page offering choice of several values and allowing to automatically 
+    select next page according to choice.
+    """
+    def __init__(self, parent, title, text, choices, helps, nexts = None):
+        Wammu.Wizard.SimplePage.__init__(self, parent, title)
+        self.type_rb = wx.RadioBox(self, -1, text,
+                size = (400, -1),
+                majorDimension = 1,
+                choices = choices)
+        self.texts = helps
+        self.nexts = nexts
+        self.Bind(wx.EVT_RADIOBOX, self.OnTypeChange, self.type_rb)
+        self.sizer.Add(self.type_rb, 0, wx.ALL, 5)
+        self.body = wx.StaticText(self, -1, self.texts[0])
+        self.body.Wrap(400)
+        self.sizer.Add(self.body, 0, wx.ALL, 5)
+
+    def OnTypeChange(self, evt):
+        try:
+            self.body.SetLabel(self.texts[evt.GetSelection()])
+            self.body.Wrap(400)
+        except:
+            self.body.SetLabel('')
+
+    def GetType(self):
+        return self.type_rb.GetSelection()
+
+    def GetNext(self):
+        if self.nexts is None:
+            return self.next
+        return self.nexts[self.type_rb.GetSelection()]
+
---
+++ wammu-configure.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+'''
+Wammu - Phone manager
+Execution script for configuration
+'''
+__author__ = 'Michal Čihař'
+__email__ = 'michal@cihar.com'
+__license__ = '''
+Copyright (c) 2003 - 2006 Michal Čihař
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License version 2 as published by
+the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+more details.
+
+You should have received a copy of the GNU General Public License along with
+this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+'''
+
+import os
+import gettext
+import sys
+import getopt
+import Wammu
+
+# Try to import iconv_codec to allow working on chinese windows
+try:
+    import iconv_codec
+except:
+    pass
+
+gettext.install('wammu', unicode=1)
+
+def version():
+    print _('Wammu - Windowed Gammu version %s') % Wammu.__version__
+
+def usage():
+    version()
+    print _('Usage: wammu [OPTION...]')
+    print
+    print _('Options:')
+    print _('-h/--help          ... show this help')
+    print _('-v/--version       ... show program version')
+    print _('-l/--local-locales ... use locales from current directory rather than system ones')
+    print
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:], 'hvl', ['help', 'version', 'local-locales'])
+except getopt.GetoptError, val:
+    usage()
+    print _('Command line parsing failed with error:')
+    print val
+    sys.exit(2)
+
+if len(args) != 0:
+    usage()
+    print _('Extra unrecognized parameters passed to program')
+    sys.exit(3)
+
+for o, a in opts:
+    if o in ('-l', '--local-locales'):
+        gettext.install('wammu','./locale/', unicode=1)
+    if o in ('-h', '--help'):
+        usage()
+        sys.exit()
+    if o in ('-v', '--version'):
+        version()
+        sys.exit()
+
+# need to be imported after locales are initialised
+import Wammu.PhoneWizard
+app = Wammu.PhoneWizard.WizardApp()
+print Wammu.PhoneWizard.RunConfigureWizard(None)
+app.Destroy()
+
