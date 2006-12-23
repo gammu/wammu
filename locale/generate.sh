10944b486c17b8b9c91e978c03eb865266969f92
Michal Čihař <michal@cihar.com>
2006-12-23 13:36:41 +0000

    [wammu @ michal@cihar.com--2005-main/wammu--devel--1--patch-218] build locales during build - remove binary message (*.mo) - add support for building and installing locales in setup.py

--- locale/generate.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-. locale/list.sh
-for loc in $LOCS ; do
-    mkdir -p locale/$loc/LC_MESSAGES
-    echo -n "$loc: "
-    msgfmt --statistics -o locale/$loc/LC_MESSAGES/wammu.mo locale/$loc.po
-done
