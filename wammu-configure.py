843206b2a25d228e80a1bd153300af0ccc76504f
Michal Čihař <michal@cihar.com>
2007-02-08 11:04:17 +0000

    [wammu @ michal@cihar.com--2005-main/wammu--devel--1--patch-259] fix local locales support

--- wammu-configure.py
+++ wammu-configure.py
@@ -70,7 +70,8 @@ if len(args) != 0:
 
 for o, a in opts:
     if o in ('-l', '--local-locales'):
-        gettext.install('wammu','./locale/', unicode=1)
+        gettext.install('wammu',os.path.join('build', 'share', 'locale'), unicode=1)
+        print _('Using local locales!')
     if o in ('-h', '--help'):
         usage()
         sys.exit()
