e4b9fc2aefff5c6713ea1eebfbc24943e54f353c
Michal Čihař <michal@cihar.com>
2007-05-12 16:53:18 +0000

    Do not print information about log in Windows, it creates new window.

--- Wammu/Main.py
+++ Wammu/Main.py
@@ -396,7 +396,8 @@ class WammuFrame(wx.Frame):
             # set filename to be used for error reports
             Wammu.ErrorLog.DEBUG_LOG_FILENAME = self.logfilename
 
-            print 'Debug log created in %s, in case of crash please include it in bugreport!' % self.logfilename
+            if sys.platform != 'win32':
+                print 'Debug log created in %s, in case of crash please include it in bugreport!' % self.logfilename
 
             self.logfilefd = os.fdopen(fd, 'w+')
             # use temporary file for logs
