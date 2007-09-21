eeedd550200e4832eef794fca6760415942502d6
Michal Čihař <michal@cihar.com>
2007-09-21 07:29:00 +0000

    Detect numbers with international prefix and without +.

--- Wammu/Utils.py
+++ Wammu/Utils.py
@@ -135,6 +135,10 @@ def NormalizeNumber(number):
     # Handle 00 prefix same as +
     if resnumber[0:2] == '00':
         resnumber = '+' + resnumber[2:]
+    # Detect numbers with international prefix and without +
+    # This can be natinal number in some countries (eg. US)
+    if NumberPrefix[0] == '+' and resnumber[:len(NumberPrefix) - 1] == NumberPrefix[1:]:
+        resnumber = '+' + resnumber
     # Add international prefix
     if resnumber[0] != '+':
         resnumber = NumberPrefix + resnumber
