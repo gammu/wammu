fc4b7aa4a4cf1e4841ddb690ffacd029bd4f23e8
Michal Čihař <michal@cihar.com>
2007-11-12 02:33:18 +0000

    Drop not used LogDialog class.

--- Wammu/PhoneSearch.py
+++ Wammu/PhoneSearch.py
@@ -51,40 +51,6 @@ except ImportError:
     except ImportError:
         BLUETOOTH = None
 
-class LogDialog(wx.Dialog):
-    def __init__(self, parent, msg = '', caption = _('Phone searching log'), pos = wx.DefaultPosition, size = (500,300)):
-        wx.Dialog.__init__(self, parent, -1, caption, pos, size)
-        x, y = pos
-        if x == -1 and y == -1:
-            self.CenterOnScreen(wx.BOTH)
-        text = wx.TextCtrl(self, -1, msg, wx.DefaultPosition,
-                             wx.DefaultSize,
-                             wx.TE_MULTILINE | wx.TE_READONLY)
-        ok = wx.Button(self, wx.ID_OK, _('Close'))
-        text.SetConstraints(wx.lib.layoutf.Layoutf('t=t5#1;b=t5#2;l=l5#1;r=r5#1', (self,ok)))
-        ok.SetConstraints(wx.lib.layoutf.Layoutf('b=b5#1;x%w50#1;w!80;h!25', (self,)))
-        self.SetAutoLayout(1)
-        self.Layout()
-        self.text = text
-        self.ok = ok
-        self.ok.Enable(False)
-        Wammu.Events.EVT_TEXT(self, self.OnText)
-        Wammu.Events.EVT_DONE(self, self.OnDone)
-        wx.EVT_CLOSE(self, self.CloseWindow)
-        self.canclose = False
-
-    def CloseWindow(self, event):
-        if self.canclose:
-            self.EndModal(wx.ID_CANCEL)
-
-    def OnText(self, evt):
-        self.text.AppendText(StrConv(evt.text))
-
-    def OnDone(self, evt):
-        self.ok.Enable(True)
-        self.canclose = True
-
-
 class AllSearchThread(threading.Thread):
     def __init__(self, lock = 'no', level = 'nothing', msgcallback = None, callback = None, win = None, noticecallback = None):
         threading.Thread.__init__(self)
