021bc033ac7310b87bdb86c969b8992455fdae40
Michal Čihař <michal@cihar.com>
2008-07-22 13:52:01 +0000

    This hack should not be needed for py2exe 0.6.5 and newer.

--- setup.py
+++ setup.py
@@ -344,22 +344,6 @@ if sys.version_info >= (2, 5):
     py2exepackages.append('email')
     py2exepackages.append('email.mime')
 
-# ModuleFinder can't handle runtime changes to __path__, but win32com uses them
-try:
-    # if this doesn't work, try import modulefinder
-    import py2exe.mf as modulefinder
-    import win32com
-    for p in win32com.__path__[1:]:
-        modulefinder.AddPackagePath("win32com", p)
-    for extra in ["win32com.shell"]: #,"win32com.mapi"
-        __import__(extra)
-        m = sys.modules[extra]
-        for p in m.__path__[1:]:
-            modulefinder.AddPackagePath(extra, p)
-except ImportError:
-    # no build path setup, no worries.
-    pass
-
 addparams = {}
 
 if HAVE_PY2EXE:
