1d3598ac7e8e7d9e9e75b544c971f998afc5b4b0
Michal Čihař <michal@cihar.com>
2008-08-29 23:06:47 +0000

    Fix exception when NumberPrefix is empty.

--- Wammu/Utils.py
+++ Wammu/Utils.py
@@ -149,7 +149,7 @@ def NormalizeNumber(number):
         resnumber = '+' + resnumber[2:]
     # Detect numbers with international prefix and without +
     # This can be national number in some countries (eg. US)
-    if NumberPrefix[0] == '+' and resnumber[:len(NumberPrefix) - 1] == NumberPrefix[1:]:
+    if NumberPrefix[0] == '+' and len(NumberPrefix) > 0 and resnumber[:len(NumberPrefix) - 1] == NumberPrefix[1:]:
         resnumber = '+' + resnumber
     # Add international prefix
     if resnumber[0] != '+':
