2db1d537dfd3c809e289f8bcb10c3326915d70a5
Michal Čihař <michal@cihar.com>
2009-10-30 09:16:34 +0100

    Update Czech translation.

---
+++ README.cs
@@ -0,0 +1,54 @@
+Wammu
+=====
+
+GUI pro Gammu.
+
+Domovská stránka
+================
+
+<http://cs.wammu.eu/>
+
+Licence
+=======
+
+GNU GPL verze 2.
+
+První spuštění
+==============
+
+Při prvním startu budete požádáni o nastavení připojení k telefonu. Pokud
+jste ještě nepoužili Gammu/Wammu, bude vám navrhnuto vyhledání telefonu,
+které by mělo ve většině případů nalézt správnou konfiguraci.
+
+Použití
+=======
+
+First you have to connect to phone, then you can perform some operations
+with it. For creating entries and importing you do not need to read things
+from phone, for others you have to (surprising? :-)).
+
+All actions with current list are accessible from context menu on each item,
+you can also use keys: Enter for editing and Delete for deleting.
+
+Backup from main menu creates backup of entries you have already retrieved
+from phone.
+
+Hlášení chyb
+============
+
+Nalezené chyby prosím nahlašte na <http://bugs.wammu.eu>.
+
+Překládání
+==========
+
+Můžete pomoci přeložit Wammu do jazyka, který umíte -
+<https://l10n.cihar.com/projects/wammu/>.
+
+Správa revizí
+=============
+
+Vývoj probíhá v Gitu, hlavní vývojovou větev naleznete na
+<git://gitorious.org/wammu/mainline.git> a prohlížet si jí můžete na
+stránkách <http://gitorious.org/wammu/mainline/trees>.
+
+# vim: et ts=4 sw=4 sts=4 tw=72 spell spelllang=en_us
