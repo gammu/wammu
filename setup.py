3c2a795f97bd3a30a6390429be807e6f6cdca013
Michal Čihař <michal@cihar.com>
2010-03-30 12:38:15 +0200

    Regenerate translated docs and install italian one.

--- setup.py
+++ setup.py
@@ -350,6 +350,7 @@ data_files.append((os.path.join('share','man','man1'), ['wammu.1', 'wammu-config
 data_files.append((os.path.join('share','man','cs','man1'), ['man/cs/wammu.1', 'man/cs/wammu-configure.1']))
 data_files.append((os.path.join('share','man','de','man1'), ['man/de/wammu.1', 'man/de/wammu-configure.1']))
 data_files.append((os.path.join('share','man','nl','man1'), ['man/nl/wammu.1', 'man/nl/wammu-configure.1']))
+data_files.append((os.path.join('share','man','it','man1'), ['man/it/wammu.1', 'man/it/wammu-configure.1']))
 
 distutils.core.setup(name="wammu",
     version = Wammu.__version__,
