cf2b9c5f79dadbcaad2acd7d7574ce80d471c49a
Michal Čihař <michal@cihar.com>
2015-06-08 15:16:52 +0200

    Clarify copyright headers and move them to comments
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- Wammu/SMSXML.py
+++ Wammu/SMSXML.py
@@ -1,26 +1,25 @@
 # -*- coding: UTF-8 -*-
+#
+# Copyright © 2008 Florent Kaisser <florent.kaisser@free.fr>
+# Copyright © 2003 - 2015 Michal Čihař <michal@cihar.com>
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 2 as published by
+# the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+#
 # vim: expandtab sw=4 ts=4 sts=4:
 '''
 Wammu - Phone manager
 Module for writing SMS to XML.
-'''
-__author__ = 'Florent Kaisser'
-__email__ = 'florent.kaisser@free.fr'
-__license__ = '''
-Copyright © 2008 Florent Kaisser
-
-This program is free software; you can redistribute it and/or modify it
-under the terms of the GNU General Public License version 2 as published by
-the Free Software Foundation.
-
-This program is distributed in the hope that it will be useful, but WITHOUT
-ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
-more details.
-
-You should have received a copy of the GNU General Public License along with
-this program; if not, write to the Free Software Foundation, Inc.,
-51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 '''
 
 from Wammu.Utils import SearchNumber
--- Wammu/WammuSettings.py
+++ Wammu/WammuSettings.py
@@ -1,4 +1,20 @@
 # -*- coding: UTF-8 -*-
+#
+# Copyright © 2003 - 2015 Michal Čihař <michal@cihar.com>
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 2 as published by
+# the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+#
 # vim: expandtab sw=4 ts=4 sts=4:
 '''
 Wammu - Phone manager
@@ -6,24 +22,6 @@ Config handler wrapper with various defaults, which might be platform dependant.
 
 @var DEFAULT_CONFIG: Dictionary of default values.
 @var EXPANDABLE_CONFIGS: List of variables where path expansion should happen.
-'''
-__author__ = 'Michal Čihař'
-__email__ = 'michal@cihar.com'
-__license__ = '''
-Copyright © 2003 - 2010 Michal Čihař
-
-This program is free software; you can redistribute it and/or modify it
-under the terms of the GNU General Public License version 2 as published by
-the Free Software Foundation.
-
-This program is distributed in the hope that it will be useful, but WITHOUT
-ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
-more details.
-
-You should have received a copy of the GNU General Public License along with
-this program; if not, write to the Free Software Foundation, Inc.,
-51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 '''
 
 import os
